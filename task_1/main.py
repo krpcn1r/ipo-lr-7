68f6e73a73672fb31aebc9c6bdd8cbb4dc873ace
krpcn1r <zaystep09@gmail.com>
2025-10-13 15:33:30 +0300

    Create main.py

---
+++ task_1/main.py
@@ -0,0 +1,8 @@
+book = []
+for i in range(1,6):
+    book.append(dict(title = input("Введите название: "), author = input("Введите автора: "), year = input("Введите год: "))) # Создаю список словарей книг
+
+for i in range(len(book)):
+    print("-" * 20, f"Книга{" " + str(1 + i)}", "-" * 20) 
+    print(f"Название: {book[i]["title"]}, Автор: {book[i]["author"]}") # Вывод данных
+    print("-" * 20, f"{book[i]["year"]}", "-" * 20)
