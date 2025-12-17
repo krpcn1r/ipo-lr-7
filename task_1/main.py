8744bd083a044f09ed9f275a8d82a48806ad3e6c
krpcn1r <zaystep09@gmail.com>
2025-12-17 20:37:12 +0300

    Update main.py

--- task_1/main.py
+++ task_1/main.py
@@ -1,4 +1,5 @@
 # Вариант 2, Зайцев
+print("start code …")
 book = []
 for i in range(1,6):
     book.append(dict(title = input("Введите название: "), author = input("Введите автора: "), year = input("Введите год: "))) # Создаю список словарей книг
@@ -7,3 +8,4 @@ for i in range(len(book)):
     print("-" * 20, f"Книга{" " + str(1 + i)}", "-" * 20) 
     print(f"Название: {book[i]["title"]}, Автор: {book[i]["author"]}") # Вывод данных
     print("-" * 20, f"{book[i]["year"]}", "-" * 20)
+print("end code …")
